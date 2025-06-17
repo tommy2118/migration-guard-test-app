ba04473d76390da627b46111b5220166c73abc96
Test User <test@example.com>
2025-06-17 08:15:52 -0400

    Alice: Add user preferences migration

---
+++ db/migrate/20250617045958_staging_migration.rb
@@ -0,0 +1,4 @@
+class StagingMigration < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
---
+++ db/migrate/20250617121536_alice_add_user_preferences.rb
@@ -0,0 +1,4 @@
+class AliceAddUserPreferences < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
